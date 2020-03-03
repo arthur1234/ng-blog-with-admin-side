ed7a29b58c13cb3ee8fa5d7a91df4d3811dfde05
arthur12324 <kidumaikido@gmail.com>
2020-03-03 05:32:07 +0200

    Login Authorisation using firebase api key

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {User} from '../shared/interfaces';
+import {User} from '../../shared/interfaces';
+import {AuthService} from '../shared/services/auth.service';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-login-page',
@@ -10,8 +13,12 @@ import {User} from '../shared/interfaces';
 export class LoginPageComponent implements OnInit {
 
   form: FormGroup;
+  submited = false;
 
-  constructor() { }
+  constructor(
+     public auth: AuthService,
+     private router: Router
+  ) { }
 
   ngOnInit() {
     this.form = new FormGroup({
@@ -31,9 +38,18 @@ export class LoginPageComponent implements OnInit {
       return;
     }
 
+    this.submited = true;
+
     const user: User = {
         email: this.form.value.email,
         password: this.form.value.password
     };
+    this.auth.login(user).subscribe(() => {
+        this.form.reset();
+        this.router.navigate(['/admin', 'dashboard']);
+        this.submited = false;
+    }, () => {
+        this.submited = false;
+    });
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyC2pAg_VLjAOLJWgzzBPDuKLHLC9ZdBmcY'
 };
